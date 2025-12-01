4987fa91ba33306a52f5f015c99d942520585c45
Matin <mtn.f1384@gmail.com>
2025-12-01 21:28:45 +0330

    Update MainWindow UI layout and styling

--- DNSChanger/MainWindow.xaml.cs
+++ DNSChanger/MainWindow.xaml.cs
@@ -1,24 +1,24 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Net.NetworkInformation;
+using System.Text.Json;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace DNSChanger
+namespace DnsChanger
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private string savePath = "saved_dns.json";
+
         public MainWindow()
         {
             InitializeComponent();
+
         }
+
+
+
     }
 }
